524292ab4ec1a043a133836356a8e2a96c58d26b
DucMinhHuy <ducminhhuy99tbvn2@gmail.com>
2022-10-26 11:50:40 +0700

    aop

---
+++ customer-management/src/main/java/cg/wbd/grandemonstration/exception/DuplicateEmailException.java
@@ -0,0 +1,8 @@
+package cg.wbd.grandemonstration.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class DuplicateEmailException extends Exception{
+
+}
